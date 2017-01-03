379c6d8ca0ce4ba8910ec856bbf35c8de07642e2
alexthomas1 <alexthomas@Alexs-MacBook-Pro-6.local>
2017-01-03 13:45:37 -0800

    Fixed this shit.

--- java/com/example/alexthomas/myapplication/MainActivity_aboutus.java
+++ java/com/example/alexthomas/myapplication/MainActivity_aboutus.java
@@ -24,7 +24,7 @@ public class MainActivity_aboutus extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_aboutus, menu);
+        getMenuInflater().inflate(R.menu.menu_set_alarm, menu);
         return true;
     }
 
--- java/com/example/alexthomas/myapplication/settings_spinners.java
+++ java/com/example/alexthomas/myapplication/settings_spinners.java
@@ -58,7 +58,7 @@ public class settings_spinners extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_set_alarm, menu);
+        getMenuInflater().inflate(R.menu.menu_aboutus, menu);
         return true;
     }
 
